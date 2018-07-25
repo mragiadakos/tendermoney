8288750c2a885b2ac23b4e975e744ca42afdc4c4
manos <manos.ragiadakos@gmail.com>
2018-07-25 20:14:16 +0300

    Updating to tendermint v0.22.6

--- docker-server/build-image.sh
+++ docker-server/build-image.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # remove previous
-rm tendermint_0.21.0_linux_amd64.zip
+rm tendermint_0.22.6_linux_amd64.zip
 rm tendermint
 rm tnmd
 rm -rf init
 
 # download tendermint
-wget https://github.com/tendermint/tendermint/releases/download/v0.21.0/tendermint_0.21.0_linux_amd64.zip .
+wget https://github.com/tendermint/tendermint/releases/download/v0.22.6/tendermint_0.22.6_linux_amd64.zip .
 # unzip the file
-unzip tendermint_0.21.0_linux_amd64.zip -d .
+unzip tendermint_0.22.6_linux_amd64.zip -d .
 
 # remove the zip
-rm tendermint_0.21.0_linux_amd64.zip
+rm tendermint_0.22.6_linux_amd64.zip
 
 CGO_ENABLED=0 GOOS=linux go build -o tnmd -a -ldflags '-extldflags "-static"' ../cmd/tnmd/ 
 
